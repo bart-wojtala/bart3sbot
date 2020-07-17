2cf6f0c999cb28fe65666ff856d417cfdf685b52
bart-wojtala <bartlomiej.wojtala@gmail.com>
2020-07-17 21:18:25 +0200

    Move http server into separate file

--- bot.js
+++ bot.js
@@ -1,25 +1,3 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = 3000;
-
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-io.on('connection', (socket) => {
-  socket.on('new message', (data) => {
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-});
-
 const tmi = require('tmi.js');
 
 const opts = {
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  socket.on('new message', (data) => {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+});
