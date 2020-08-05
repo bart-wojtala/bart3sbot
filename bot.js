ea59398d3334ad784bcc94bcbe73f6c000d0d4c0
Bartłomiej Wojtala <bartlomiej.wojtala@gmail.com>
2020-08-05 15:41:57 +0200

    Update automatic message

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ function sendFollowAlert() {
 }
 
 function sendCommandsAlert() {
-  client.say("bart3s", "Available commands: !tts !help !voices !emotes");
+  client.say("bart3s", "Available commands: !tts !help !voices !bttv !ffz");
 }
 
 setInterval(sendCommandsAlert, 1.5 * 60 * 1000);
