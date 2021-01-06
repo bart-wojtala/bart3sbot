95f1f984649d32feced10117afc1b81f4a3296f6
bart-wojtala <bartlomiej.wojtala@gmail.com>
2021-01-06 14:38:16 +0100

    Sort model list in alphabetical order

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const opts = {
     ffz: process.env.EMOTES_FFZ
   },
   tts: {
-    voices: ['woman:', 'david:', 'neil:', 'stephen:', 'satan:', 'voicemail:', 'vader:', 'trump:', 'gandalf:', 'keanu:', 'mszira:', 'msdavid:', 'samuel:', 'hal:'],
+    voices: ['david:', 'gandalf:', 'hal:', 'keanu:', 'msdavid:', 'mszira:', 'neil:', 'samuel:', 'satan:', 'stephen:', 'trump:', 'vader:', 'woman:', 'voicemail:'],
     timeout: process.env.TTS_TIMEOUT_SECONDS
   },
   advertisement: {
