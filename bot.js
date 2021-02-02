d891c10400b8c044fa4d2af0f3fc9b65463393d0
bart-wojtala <bartlomiej.wojtala@gmail.com>
2021-02-02 11:05:08 +0100

    Refactor code

--- bot.js
+++ bot.js
@@ -39,48 +39,48 @@ function onMessageHandler(target, context, msg, self) {
   messageTime = new Date();
   timestamp = messageTime.getTime();
   const commandName = msg.trim();
-  name = context['display-name']
+  displayName = context['display-name']
 
   if (commandName.startsWith("!tts")) {
-    if (name) {
-      lastUserTimestamp = userTimestampMap.get(name);
+    if (displayName) {
+      lastUserTimestamp = userTimestampMap.get(displayName);
       timeDifference = timestamp - lastUserTimestamp;
 
-      if (name !== adminName && lastUserTimestamp && timeDifference < ttsTimeout) {
-        client.say(target, `${name} you have to wait ${Math.round((ttsTimeout - timeDifference) / 1000)} seconds to send next TTS message!`);
+      if (displayName !== adminName && lastUserTimestamp && timeDifference < ttsTimeout) {
+        client.say(target, `${displayName} you have to wait ${Math.round((ttsTimeout - timeDifference) / 1000)} seconds to send next TTS message!`);
       } else {
         if (commandName.length < 6) {
-          client.say(target, `${name} wrong command usage! Check instructions on a panel below the stream window.`);
+          client.say(target, `${displayName} wrong command usage! Check instructions on a panel below the stream window.`);
         } else {
           message = commandName.substring(5)
           messageLength = message.length
           if (messageLength > 255) {
-            client.say(target, `${name} message length: ${messageLength} exceeds the character limit!`);
+            client.say(target, `${displayName} message length: ${messageLength} exceeds the character limit!`);
           } else {
             sendTTSMessage(context.id, context.username, message, messageTime.toLocaleTimeString());
-            userTimestampMap.set(name, timestamp);
-            client.say(target, `${name} your message is added to the queue.`);
+            userTimestampMap.set(displayName, timestamp);
+            client.say(target, `${displayName} your message is added to the queue.`);
           }
         }
       }
     }
-  } else if (commandName.startsWith("!set ttstimeout") && name === adminName) {
+  } else if (commandName.startsWith("!set ttstimeout") && displayName === adminName) {
     newTimeout = commandName.substring(16);
     ttsTimeout = parseInt(newTimeout) * 1000;
     client.say(target, "New TTS timeout: " + newTimeout + " seconds.");
   } else if (commandName === "!help") {
-    client.say(target, `${name} TTS instructions are available on the panel below the stream. You can also use command !test to generate a test message using random voice.`);
+    client.say(target, `${displayName} TTS instructions are available on the panel below the stream. You can also use command !test to generate a test message using random voice.`);
   } else if (commandName === "!test") {
-    lastUserTimestamp = userTimestampMap.get(name);
+    lastUserTimestamp = userTimestampMap.get(displayName);
     timeDifference = timestamp - lastUserTimestamp;
 
-    if (name !== adminName && lastUserTimestamp && timeDifference < ttsTimeout) {
-      client.say(target, `${name} just let me rest for ${Math.round((ttsTimeout - timeDifference) / 1000)} seconds...`);
+    if (displayName !== adminName && lastUserTimestamp && timeDifference < ttsTimeout) {
+      client.say(target, `${displayName} just let me rest for ${Math.round((ttsTimeout - timeDifference) / 1000)} seconds...`);
     } else {
       var randomVoice = opts.tts.voices[Math.floor(Math.random() * opts.tts.voices.length)];
       message = `${randomVoice} This is a test. 1, 2 and 3.`
       sendTTSMessage(context.id, opts.identity.username, message, messageTime.toLocaleTimeString());
-      userTimestampMap.set(name, timestamp);
+      userTimestampMap.set(displayName, timestamp);
       client.say(target, '!tts ' + message);
       client.say(target, `I added this message to the queue.`);
     }
